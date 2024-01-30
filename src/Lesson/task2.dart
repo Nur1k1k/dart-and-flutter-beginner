06aec32fdaeaf309b695e5f417cffadb60eb83c6
Nurkozha <nrkdk@mail.ru>
2024-01-30 09:03:50 +0600

    third

--- src/Lesson/task2.dart
+++ src/Lesson/task2.dart
@@ -1,21 +1,16 @@
 import 'dart:io';
 
 void main(){
-  // Создаем поток ввода для чтения данных с консоли
+
   var input = stdin;
-
-  // Запрашиваем у пользователя ввод строки
   print('Введите строку:');
-  String userInput = input.readLineSync() ?? ''; // Используем оператор ?? для предотвращения null
-
-  // Переворачиваем строку
+  String userInput = input.readLineSync() ?? '';
   String reversedString = reverseString(userInput);
 
-  // Выводим результат
   print('Перевернутая строка: $reversedString');
 }
 
-// Функция для переворачивания строки
+
 String reverseString(String input) {
   return String.fromCharCodes(input.runes.toList().reversed);
 }
