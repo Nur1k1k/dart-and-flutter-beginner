b485d45b202b3ce9b48e610910496ddb7af2ee18
Nurkozha <nrkdk@mail.ru>
2024-02-02 17:55:00 +0600

    lab1

---
+++ src/Lesson/lab1/task1.dart
@@ -0,0 +1,47 @@
+
+import 'dart:ffi';
+import 'dart:io';
+
+void main(){
+  var num = stdin;
+
+  print('Введите первое число:');
+  // Используем int.parse для преобразования строки в число
+  int number1 = int.parse(num.readLineSync() ?? '0');
+  print('Введите оператор:');
+  String s = num.readLineSync() ?? '';
+  switch(s){
+    case "+":
+      print('Введите второе число:');
+      int number2 = int.parse(num.readLineSync() ?? '0');
+      int sum = number1+number2;
+      print('Result: $sum');
+      break;
+
+    case "-":
+      print('Введите второе число:');
+      int number2 = int.parse(num.readLineSync() ?? '0');
+      int sum = number1-number2;
+      print('Result: $sum');
+      break;
+
+    case "*":
+      print('Введите второе число:');
+      int number2 = int.parse(num.readLineSync() ?? '0');
+      int sum = number1*number2;
+      print('Result: $sum');
+      break;
+
+    case "/":
+      print('Введите второе число:');
+      int number2 = int.parse(num.readLineSync() ?? '0');
+      if(number2==0)
+        break;
+      double sum = number1 /number2;
+      print('Result: $sum');
+      break;
+  }
+
+
+
+}
